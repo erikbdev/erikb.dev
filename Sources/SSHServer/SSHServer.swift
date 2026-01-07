18f3208cec5b673b2bedd936527186b456a6ddf9
Erik Bautista Santibanez <me@erikb.dev>
2026-01-07 00:40:45 -0800

    wip

--- Sources/SSHServer/SSHServer.swift
+++ Sources/SSHServer/SSHServer.swift
@@ -3,6 +3,7 @@ import Crypto
 import Foundation
 import Logging
 @preconcurrency import NIO
+import NIOCore
 @preconcurrency import NIOSSH
 
 let log = {
@@ -43,33 +44,81 @@ struct SSHServer: AsyncParsableCommand {
       .serverChannelOption(.backlog, value: 256)
       .serverChannelOption(.socketOption(.so_reuseaddr), value: 1)
       // .serverChannelOption(.socketOption(.tcp_nodelay), value: 1)
-      .childChannelInitializer { channel in
-        channel.pipeline.eventLoop.makeCompletedFuture {
-          try channel.pipeline.syncOperations.addHandler(
-            NIOSSHHandler(
-              role: .server(
-                SSHServerConfiguration(
-                  hostKeys: [hostKey],
-                  userAuthDelegate: delegate,
-                  globalRequestDelegate: nil,
-                  banner: nil
-                )
-              ),
-              allocator: channel.allocator,
-            ) { child, type in
-              child.pipeline.addHandler(InteractiveClient(type: type))
+      .childChannelOption(.allowRemoteHalfClosure, value: true)
+    // .childChannelInitializer { channel in
+    //   channel.pipeline.eventLoop.makeCompletedFuture {
+    //     try channel.pipeline.syncOperations.addHandler(
+    //       NIOSSHHandler(
+    //         role: .server(
+    //           SSHServerConfiguration(
+    //             hostKeys: [hostKey],
+    //             userAuthDelegate: delegate,
+    //             globalRequestDelegate: nil,
+    //             banner: nil
+    //           )
+    //         ),
+    //         allocator: channel.allocator,
+    //       ) { child, type in
+    //         child.pipeline.addHandler(InteractiveClient(type: type))
+    //       }
+    //     )
+    //   }
+    // }
+
+    let serverChannel = try await bootstrap.bind(host: host, port: port) { channel in
+      channel.pipeline.eventLoop.makeCompletedFuture {
+        try channel.pipeline.syncOperations.addHandler(
+          NIOSSHHandler(
+            role: .server(
+              SSHServerConfiguration(
+                hostKeys: [hostKey],
+                userAuthDelegate: delegate,
+                globalRequestDelegate: nil,
+                banner: nil
+              )
+            ),
+            allocator: channel.allocator,
+          ) { channel, type in
+            channel.eventLoop.makeSucceededVoidFuture()
+          }
+        )
+        return try NIOAsyncChannel<IOData, IOData>(wrappingChannelSynchronously: channel)
+      }
+    }
+
+    log.info("SSH Server listening on \(host):\(port)")
+
+    try await serverChannel.executeThenClose { inbound in
+      try await withThrowingDiscardingTaskGroup { group in
+        for try await client in inbound {
+          group.addTask {
+            try await client.executeThenClose { clientInbound, clientOutbound in
+              log.info("SSH Client connected [\(client.channel.remoteAddress?.ipAddress ?? "")")
+
+              try await withThrowingTaskGroup { group in
+              // try await clientOutbound.write(.byteBuffer(ByteBuffer(string: "jhello")))
+
+                // try await clientOutbound.write(.init(type: .channel, data: .byteBuffer(ByteBuffer(string: "Hello, Terminal!"))))
+                group.addTask {
+                  for try await input in clientInbound {
+                    let string: String
+
+                    switch input {
+                    case .byteBuffer(let buffer):
+                      string = String(buffer: buffer)
+                    case .fileRegion(let file):
+                      let fileIo = NonBlockingFileIO(threadPool: .singleton)
+                      string = (try? await String(buffer: fileIo.read(fileRegion: file, allocator: client.channel.allocator))) ?? ""
+                    }
+                    log.debug("New input data: \(string)")
+                  }
+                }
+                try await client.channel.closeFuture.get()
+              }
             }
-          )
+          }
         }
       }
-      .childChannelOption(.allowRemoteHalfClosure, value: true)
-
-    let serverChannel = try await bootstrap
-      .bind(host: host, port: port)
-      .get()
-
-    log.debug("SSH server listening on \(host):\(port))")
-    try await serverChannel.closeFuture.get()
-    try await serverChannel.eventLoop.shutdownGracefully()
+    }
   }
 }
