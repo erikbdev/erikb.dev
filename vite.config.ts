c8430121445a27ed29e0b35ba5d2761897bfbf11
Erik Bautista Santibanez <me@erikb.dev>
2026-04-08 01:02:06 -0700

    wip: add vue vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite-plus";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
   root: "./Sources/SiteApp",
-  base: "./../..",
+  plugins: [vue(), vueDevTools()],
   publicDir: "./../../public",
   build: {
     outDir: "./../../dist",
+  },
+  resolve: {
+    alias: {
+      "@": ".",
+    }
   },
   fmt: {},
   lint: { 
