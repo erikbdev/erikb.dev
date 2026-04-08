1d1231dce9bb69210c6723f415bbb8a7e6d0a025
Erik Bautista Santibanez <me@erikb.dev>
2026-04-08 01:08:28 -0700

    wip

--- Sources/SiteApp/env.d.ts
+++ Sources/SiteApp/env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite-plus/client" />
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
