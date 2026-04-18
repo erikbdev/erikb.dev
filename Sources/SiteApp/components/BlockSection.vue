c395af7122b1934b2f420394b8f79f35966d4863
Erik Bautista Santibanez <me@erikb.dev>
2026-04-18 13:51:29 -0700

    wip: improve border

--- Sources/SiteApp/components/BlockSection.vue
+++ Sources/SiteApp/components/BlockSection.vue
@@ -1,31 +1,34 @@
 <script lang="ts">
-import type { PropType } from 'vue';
+import type { PropType } from "vue";
 export default {
   props: {
     tag: {
       type: String,
-      default: "div"
+      default: "div",
     },
     fill: {
-      type: String as PropType<"inner" | boolean | "">,
-      default: false
-    }
-  }
-}
+      type: [String, Boolean] as PropType<"inner" | boolean | "">,
+      default: false,
+    },
+  },
+};
 </script>
 <template>
-  <component :is="$props.tag || 'div'" class="border-b border-[#303030]">
-    <div 
-      :class="[
-        'border-x border-[#303030]',
-        ($props.fill === true || $props.fill === '') ? '' : [
-          'md:max-w-2xl mx-auto',
-          {
-            'px-6 py-3': $props.fill !== 'inner',
-          }
-        ]
-      ]">
-      <slot></slot>
-    </div>
+  <component
+    :is="$props.tag || 'div'"
+    :class="[
+      'relative border-[#303030]',
+      'after:absolute after:bottom-0 after:left-1/2 after:w-screen after:-translate-x-1/2 after:h-px after:bg-[#303030] ',
+      $props.fill === true || $props.fill === ''
+        ? ''
+        : [
+            'md:border-x md:max-w-2xl mx-auto',
+            {
+              'px-6 py-3': $props.fill !== 'inner',
+            },
+          ],
+    ]"
+  >
+    <slot></slot>
   </component>
 </template>
