586eaadae099a21614b11e4b5f2895f44e1ec229
Erik Bautista Santibanez <me@erikb.dev>
2026-04-25 20:40:18 -0700

    wip: improve border divider

--- Sources/SiteApp/+Layout.vue
+++ Sources/SiteApp/+Layout.vue
@@ -10,9 +10,9 @@ const showMenuDialog = ref(false);
   <main class="relative">
     <BlockSection as="nav" fill :divider="false" class="fixed! top-0 z-50 bg-base/90 flex flex-none justify-between py-3 px-4">
       <a href="/" class="text-sm self-center">
-        <code class="text-white font-bold">erikb.dev();</code>
+        <code class="text-white font-bold">erikb.dev()</code>
       </a>
-      <button :class="`font-bold text-white text-sm border-[1.16px] border-solid border-neutral-700 py-1 px-2 cursor-pointer ${showMenuDialog ? 'bg-neutral-400 text-neutral-900' : ''}`" @click.stop="showMenuDialog = !showMenuDialog">
+      <button class="font-bold text-white text-sm border-[1.16px] border-solid border-neutral-700 py-1 px-2 cursor-pointer" :class="showMenuDialog ? 'bg-neutral-400 text-neutral-900' : ''" @click.stop="showMenuDialog = !showMenuDialog">
         <code>{{ '<menu>' }}</code>
       </button> 
     </BlockSection>
--- Sources/SiteApp/components/BlockSection.vue
+++ Sources/SiteApp/components/BlockSection.vue
@@ -31,14 +31,20 @@ export default {
               'px-4 py-4': fill !== 'inner',
             },
           ],
-      divider ? 'mb-4 after:absolute after:h-4 after:-bottom-4 after:w-screen after:md:w-2xl after:left-1/2 after:-translate-x-1/2 after:border after:border-border after:outline after:outline-border after:-outline-offset-1 divider-lines' : 'border-b ',
+      divider ? 
+        'mb-4 after:absolute after:h-4 after:-bottom-4 after:w-screen after:md:w-2xl after:left-1/2 after:-translate-x-1/2 after:border after:border-border after:outline after:outline-border after:-outline-offset-1 big-divider' : 
+        'mb-0 after:absolute after:h-0 after:bottom-0 after:w-screen after:md:w-2xl after:left-1/2 after:-translate-x-1/2 after:border-b after:border-border compact-divider',
     ]"
   >
     <slot></slot>
   </component>
 </template>
 <style>
-.divider-lines::after {
+.compact-divider::after {
+  border-image: linear-gradient(to right, #303030, #303030) 1 / 1px / 0 100vw;
+}
+
+.big-divider::after {
   border-image: linear-gradient(to right, #303030, #303030) 1 / 1px / 0 100vw;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M0 8L8 0M-2 2L2-2M6 10L10 6' stroke='%23333' stroke-width='2' stroke-linecap='square'/%3E%3C/svg%3E");
   background-size: 6px 4px;
