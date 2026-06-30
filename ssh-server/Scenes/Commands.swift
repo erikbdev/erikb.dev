941625786ff7f910af2ebee450af58b974f2d64b
Erik Bautista Santibanez <me@erikb.dev>
2026-06-30 00:57:15 -0700

    wip

--- ssh-server/Scenes/Commands.swift
+++ ssh-server/Scenes/Commands.swift
@@ -5,7 +5,7 @@ struct PortfolioCommand: Hashable, Sendable, CaseIterable {
   var command: String { self.rawValue }
 
   static let home = Self(title: "Home", rawValue: "whoami")
-  static let devlogs = Self(title: "Dev Logs", rawValue: "ls -l dev-logs")
+  static let devlogs = Self(title: "Dev Logs", rawValue: "dev-logs")
   static let exit = Self(title: "Exit", rawValue: "exit")
   static let help = Self(title: "Help", rawValue: "help")
 
--- ssh-server/Scenes/Views/PortfolioView.swift
+++ ssh-server/Scenes/Views/PortfolioView.swift
@@ -12,82 +12,93 @@ struct PortfolioView: View {
   @State private var command: PortfolioCommand = .home
   @State private var commandInput = ""
   @State private var commandNotFound = ""
+  @State private var showHelpCommand = false
   @FocusState private var commandInputFocused
 
   var body: some View {
-    Group {
-      // if showingSplash {
-      // SplashView { showingSplash = false }
-      // } else {
-      // }
+    // if showingSplash {
+    // SplashView { showingSplash = false }
+    // } else {
+    // }
+    VStack(alignment: .leading, spacing: 0) {
+      ScrollView {
+        Group {
+          Text("> \(command.rawValue)")
+          switch command {
+          case .home:
+            HomeView()
+          case .devlogs:
+            DevLogsView()
+          default:
+            EmptyView()
+          }
+        }
+
+        .frame(maxWidth: .infinity)
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+
       VStack(alignment: .leading, spacing: 0) {
-        ScrollView {
-          // TODO: make command array based, each command having its own state.
+        HStack(spacing: 0) {
+          Text("erikbdev@portfolio:$ ")
+            .foregroundStyle(.yellow)
+          TextField(text: $commandInput, prompt: Text("type a command (e.g. help)"), label: EmptyView.init)
+            .focused($commandInputFocused)
+            .textFieldStyle(.plain)
+            .onKeyPress(.return) { _ in
+              resolveCommandInput()
+            }
+            .onKeyPress(.tab) { _ in
+              .handled
+            }
+            .prefersDefaultFocus(in: .default)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+
+        if commandNotFound.count > 0 {
+          Text("command not found: \"\(commandNotFound)\", type \"help\" for commands ")
+            .foregroundStyle(.red)
+          // .padding(.top, 1)
+        }
+
+        if showHelpCommand {
           Group {
-            switch command {
-            case .home:
-              HomeView()
-            case .devlogs:
-              DevLogsView()
-            default:
-              EmptyView()
+            Text("available commands:")
+            // .padding(.top, 1)
+            ForEach(PortfolioCommand.allCases, id: \.self) { command in
+              Text("  \(command.rawValue) // \(command.title)")
             }
           }
-          // .onKeyPress(.character("/")) { _ in
-          //   guard !commandInputFocused else {
-          //     return .ignored
-          //   }
-          //   commandInputFocused = true
-          //   return .handled
-          // }
-
-          .frame(maxWidth: .infinity)
+          .foregroundStyle(Color(white: 0.5))
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-
-        VStack(alignment: .leading, spacing: 0) {
-          HStack(spacing: 0) {
-            Text("erikbdev@portfolio:$ ")
-              .foregroundStyle(.yellow)
-            TextField(text: $commandInput, prompt: Text("type a command (e.g. help)"), label: EmptyView.init)
-              .focused($commandInputFocused)
-              .textFieldStyle(.plain)
-              .onKeyPress(.return) { _ in
-                resolveCommandInput()
-              }
-              .onKeyPress(.tab) { _ in
-                .handled
-              }
-              .prefersDefaultFocus(in: .default)
-          }
-          .frame(maxWidth: .infinity, alignment: .leading)
-        }
-        .border(.yellow, set: .rounded, placement: .outset)
-
-        HStack(spacing: 0) {
-          if !commandNotFound.isEmpty {
-            Text("command not found: \"\(commandNotFound)\", type \"help\" for commands ")
-              .foregroundStyle(.red)
-          }
-          Text(
-            "\(Text("*").foregroundStyle(.white)) type exit to quit | \(Text("↑/↓").foregroundStyle(.white)) navigate | \(Text("enter").foregroundStyle(.white)) select"
-          )
-          .foregroundStyle(Color(white: 0.4))
-          .italic()
-        }
-        .padding(.leading, 1)
       }
-      .padding(0)
+      .border(commandInputFocused ? .yellow : .gray, set: .rounded, placement: .outset)
+
+      VStack(spacing: 0) {
+        if !commandNotFound.isEmpty {
+        }
+        Text(
+          "\(Text("↑/↓").foregroundStyle(.white)) navigate | \(Text("enter").foregroundStyle(.white)) select"
+        )
+        .foregroundStyle(Color(white: 0.5))
+        .italic()
+      }
+      .padding(.leading, 1)
     }
+    .padding(0)
     .environment(\.logger, logger)
     .environment(\.exitAction, exitAction)
   }
 
   private func resolveCommandInput() -> KeyPressResult {
     let trimmedTextInput = commandInput.trimmingCharacters(in: .whitespacesAndNewlines)
-    let newCommand = PortfolioCommand.allCases.first { $0.command.hasPrefix(trimmedTextInput) }
+    self.showHelpCommand = false
+    self.commandNotFound = ""
+    let newCommand = PortfolioCommand.allCases.first { $0.command == trimmedTextInput }
     if newCommand == .exit {
       self.exitAction()
+    } else if newCommand == .help {
+      showHelpCommand = true
     } else if let newCommand, self.command != newCommand {
       self.command = newCommand
     } else if newCommand == nil {
